ccbabe45b092bb9246b9892897a6eadc577842de
pahuizinga <pmhuizinga@protonmail.com>
2021-12-02 09:16:34 +0100

    day 1

--- aoc_code_1.py
+++ aoc_code_1.py
@@ -4,12 +4,13 @@ day = 1
 data = read_data(day, test=True)
 data = [int(x) for x in data]
 
+
 # part 1
 def count_up(data):
-    return sum([1 for i, j in zip(data[:-1], data[1:]) if i < j])
+    return sum(i < j for i, j in zip(data[:-1], data[1:]))
+
 
 print(count_up(data))
 
 # part 2
-print(count_up([sum(data[i-3:i]) for (i, x) in enumerate(data) if i < len(data) and i >= 2]))
-
+print(count_up([sum(data[i - 3:i]) for (i, x) in enumerate(data) if i < len(data) and i >= 2]))
