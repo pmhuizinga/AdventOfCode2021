393cb74ccd034fe0d7cf793c853fc2aee4ccaf70
pahuizinga <pmhuizinga@protonmail.com>
2021-12-09 18:14:43 +0100

    day 7

--- aoc_code_7.py
+++ aoc_code_7.py
@@ -1,4 +1,12 @@
 from general_functions import read_data
+import numpy as np
 
 day = 7
-data = read_data(day, test=False)
+data = read_data(day, test=True)
+data = [int(x) for x in data[0].split(',')]
+#%%
+
+max_pos = max(data)
+min_pos = min(data)
+avg_pos = np.mean(data)
+
