84b988f5711f8fe11c05178ef4a52ebd3a710674
pmhuizinga <pmhuizinga@hotmail.com>
2021-12-09 08:07:22 +0100

    day 6

--- aoc_code_6.py
+++ aoc_code_6.py
@@ -2,6 +2,25 @@ from general_functions import read_data
 import numpy as np
 import math
 
+# %%
+start_val = [3]
+days = 19
+freq = 7
+start_day = 8
+global nr_of_offspring
+
+
+def test(start_val, days, freq, start_day):
+    print(days - start_val)
+    if (days - start_val) > freq:
+        nr_of_offspring = math.floor((days - start_val) / freq)
+
+    return nr_of_offspring
+
+
+print(test(start_val[0], days, freq, start_day))
+
+# %%
 day = 6
 data = read_data(day, test=True)
 data = [int(x) for x in data[0].split(',')]
@@ -10,6 +29,7 @@ data = [int(x) for x in data[0].split(',')]
 def lanternfish(lst, days):
     day = 0
     arr = np.array(lst)
+    print('day: {}, arr: {}'.format(day, arr))
     while days > 0:
         day += 1
         for idx in range(len(arr)):
@@ -23,36 +43,34 @@ def lanternfish(lst, days):
 
     return list(arr)
 
+
 # %%
 start_num = 3
-period = 18
+period = 30
 print('6a:')
 result = lanternfish([start_num], period)
 print('result for 6a: {}'.format(len(result)))
 
 # %%
-import math
-
 nr_of_children = 0
-checklist = []
+
+def calc_child(start_number: int, days: int, number_of_children: int = 0) -> int:
+    if start_number <= days:
+        days_remaining = days - start_number
+        number_of_children += (math.ceil(days_remaining / 7))
+        print(number_of_children)
+        if days_remaining >= 7:
+            return calc_child(8, days_remaining, number_of_children)
+        else:
+            return number_of_children
+    # else:
+    #     return number_of_children
 
 
-def calc_child(start_num, period, nr_of_children):
-    checklist.append([start_num, period, nr_of_children])
-    if start_num <= period:
-        days_after_first_birth = period - start_num
-        nr_of_children += 1 + math.floor(days_after_first_birth / 7)
-        if days_after_first_birth > 7:
-            print('startnum: {}, days_after_first_birth: {}, nr_of_children: {}'.format(8,
-                                                                                        days_after_first_birth,
-                                                                                        nr_of_children))
-            nr_of_children = calc_child(8, days_after_first_birth, nr_of_children)
-            days_after_first_birth -= 7
+print('result {}'.format(calc_child(start_num, period)))
 
-    print(checklist)
-    return nr_of_children
-
-print(1 + calc_child(3, period, 0))
 # print(sum([(1+calc_child(x, 18, 0)) for x in [3]]))
-
-
+# period - start_num = days_remaining
+# checklist.append([start_number, days, number_of_children])
+# print('startnum: {}, days_remaining: {}, nr_of_children: {}'.format(8, days_remaining, number_of_children))
+# %%
