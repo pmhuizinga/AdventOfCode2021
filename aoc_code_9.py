f8ad8b7a15cab69f3fe49d09c14c557e449d9a20
pmhuizinga <pmhuizinga@hotmail.com>
2021-12-13 22:44:08 +0100

    day 9

--- aoc_code_9.py
+++ aoc_code_9.py
@@ -8,22 +8,27 @@ data = np.array([[int(y) for y in x] for x in data])
 rows = data.shape[0]
 cols = data.shape[1]
 
-# check if the value to the left is lower
-check_left = data[:, 1:] < data[:, :-1]
-check_left = np.c_[np.ones(rows), check_left]
-# check if the value to the right is lower
-check_right = data[:, :-1] < data[:, 1:]
-check_right = np.c_[check_right, np.ones(rows)]
-# check if the value to the top is lower
-check_top = data[1:, :] < data[:-1, :]
-check_top = np.r_[[np.ones(cols)], check_top]
-# check if the value to the bottom is lower
-check_bottom = data[:-1, :] < data[1:, :]
-check_bottom = np.r_[check_bottom, [np.ones(cols)]]
 
-total_check = check_left * check_right * check_top * check_bottom
+def get_lows(data):
+    # check if the value to the left is lower
+    check_left = data[:, 1:] < data[:, :-1]
+    check_left = np.c_[np.ones(rows), check_left]
+    # check if the value to the right is lower
+    check_right = data[:, :-1] < data[:, 1:]
+    check_right = np.c_[check_right, np.ones(rows)]
+    # check if the value to the top is lower
+    check_top = data[1:, :] < data[:-1, :]
+    check_top = np.r_[[np.ones(cols)], check_top]
+    # check if the value to the bottom is lower
+    check_bottom = data[:-1, :] < data[1:, :]
+    check_bottom = np.r_[check_bottom, [np.ones(cols)]]
 
-print(sum(sum((total_check * data)+total_check)))
+    total_check = check_left * check_right * check_top * check_bottom
+    return total_check
+
+
+result = get_lows(data)
+print(sum(sum((result * data) + result)))
 
 # part 2
 # https://dragly.org/2013/03/25/working-with-percolation-clusters-in-python/
