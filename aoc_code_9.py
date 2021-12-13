63026ff843be09c7c718b0c6d9d5531acce9c0fd
pahuizinga <pmhuizinga@protonmail.com>
2021-12-13 17:25:42 +0100

    day 9

--- aoc_code_9.py
+++ aoc_code_9.py
@@ -1,4 +1,11 @@
 from general_functions import read_data
+import numpy as np
 
 day = 9
 data = read_data(day, test=True)
+data = np.array([[int(y) for y in x] for x in data])
+
+# check if the value to the left is lower
+check_left = data[:, 1:] < data[:, :-1]
+# check if the value to the right is lower
+check_right = data[:, :-1] < data[:, :-1]
