fe0b51449bde8ed6b568a1ba82d0da78904e5a21
pmhuizinga <pmhuizinga@hotmail.com>
2021-12-13 22:29:17 +0100

    day 9

--- aoc_code_9.py
+++ aoc_code_9.py
@@ -1,5 +1,6 @@
 from general_functions import read_data
 import numpy as np
+from scipy.ndimage import measurements
 
 day = 9
 data = read_data(day, test=False)
@@ -23,3 +24,12 @@ check_bottom = np.r_[check_bottom, [np.ones(cols)]]
 total_check = check_left * check_right * check_top * check_bottom
 
 print(sum(sum((total_check * data)+total_check)))
+
+# part 2
+# https://dragly.org/2013/03/25/working-with-percolation-clusters-in-python/
+data[data < 9] = 1
+data[data == 9] = 0
+lw, num = measurements.label(data)
+area = list(measurements.sum(data, lw, index=np.arange(lw.max() + 1)))
+area.sort()
+print(int(np.product(area[-3:])))
