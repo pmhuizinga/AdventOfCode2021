571daef21ea2ddc15fb57d5a23c81960231db891
pahuizinga <pmhuizinga@protonmail.com>
2021-12-14 16:58:15 +0100

    day 10

--- aoc_code_10.py
+++ aoc_code_10.py
@@ -1,16 +1,23 @@
 from general_functions import read_data
 
 day = 10
-data = read_data(day, test=True)
+data = read_data(day, test=False)
 
-chunk_bits = ['[', ']', '{', '}', '<', '>', '(', ')']
+open_bits = ['[', '{', '<', '(']
+close_bits = [']', '}', '>', ')']
+reward = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
+stack = []
 
-def question_10_a(ata):
+def question_10_a(data):
+    for char in data:
+        if char in open_bits:
+            stack.append(char)
+        if char in close_bits:
+            idx = close_bits.index(char)
+            if open_bits[idx] == stack[-1]:
+                stack.pop()
+            else:
+                return char
 
-    for row in data:
-        chunk_count = []
-        for chunk in chunk_bits:
-            chunk_count.append(row.count(chunk))
-    return chunk_count
+print(sum([reward[question_10_a(x)] for x in data]))
 
-print(question_10_a(data))
