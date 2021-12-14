79185a97646c34ca19dd4b2625c032ad104a1b9e
pmhuizinga <pmhuizinga@hotmail.com>
2021-12-14 21:27:03 +0100

    day 10

--- aoc_code_10.py
+++ aoc_code_10.py
@@ -1,11 +1,13 @@
+import numpy as np
+
 from general_functions import read_data
 
 day = 10
-data = read_data(day, test=True)
+data = read_data(day, test=False)
 
 open_bits = ['[', '{', '<', '(']
 close_bits = [']', '}', '>', ')']
-reward = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
+reward_1 = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
 
 
 def question_10_a(data):
@@ -19,9 +21,36 @@ def question_10_a(data):
                 stack.pop()
             else:
                 return char
-    print(stack)
 
-print(sum([reward[question_10_a(x)] for x in data]))
+
+print(sum([reward_1[question_10_a(x)] for x in data]))
 
 # part 2
-reward = {None: 0, ')': 1, ']': 2, '}': 3, '>': 4}
+reward_2 = {None: 0, ')': 1, ']': 2, '}': 3, '>': 4}
+mapping = {'[': ']', '{': '}', '(': ')', '<': '>'}
+
+
+def question_10_b(data):
+    stack = []
+    for char in data:
+        if char in open_bits:
+            stack.append(char)
+        if char in close_bits:
+            idx = close_bits.index(char)
+            if open_bits[idx] == stack[-1]:
+                stack.pop()
+            else:
+                return None
+    return stack
+
+out = [question_10_b(x) for x in data]
+out = [[mapping[y] for y in x[::-1]] for x in out if x is not None]
+print([x for x in out])
+
+def total_score(list):
+    score = 0
+    for x in list:
+        score = (score * 5) + reward_2[x]
+    return score
+
+print(np.median([total_score(x) for x in out]))
