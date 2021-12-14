346e6576cf3fd57fa2c2592f3982ce601a57cdd2
pmhuizinga <pmhuizinga@hotmail.com>
2021-12-14 08:50:41 +0100

    day 10

--- aoc_code_10.py
+++ aoc_code_10.py
@@ -3,5 +3,14 @@ from general_functions import read_data
 day = 10
 data = read_data(day, test=True)
 
-for row in data:
-    print(row)
+chunk_bits = ['[', ']', '{', '}', '<', '>', '(', ')']
+
+def question_10_a(ata):
+
+    for row in data:
+        chunk_count = []
+        for chunk in chunk_bits:
+            chunk_count.append(row.count(chunk))
+    return chunk_count
+
+print(question_10_a(data))
